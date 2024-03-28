926ba3b4a5b3f409a354410106e709233ac52d83
Joris Weeda <j.j.weeda@student.tudelft.nl>
2024-03-28 09:52:08 +0100

    Updated scripts in package settings

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
 
 setup_args = generate_distutils_setup(
-    packages=['control', 'environment', 'isaacgym', 'monitor', 'motion', 'scheduler', 'sockets'],
+    packages=['control', 'environment', 'isaacgym', 'monitor', 'motion', 'scheduler'],
     package_dir={'': 'src'},
-    scripts=['scripts/node_client', 'scripts/node_server', 'scripts/node_collect', 
-             'scripts/environment', 'scripts/controller', 'scripts/mimic_topic', 'scripts/visualisation'],
+    scripts=['scripts/collector', 'scripts/controller', 'scripts/environment', 
+             'scripts/mimic_topic', 'scripts/visualisation'],
 )
 
 setup(**setup_args)
