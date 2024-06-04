2fc0e632d2436d7fccdacff5b09b9c2734f3528f
Joris Weeda <j.j.weeda@student.tudelft.nl>
2024-06-04 15:49:24 +0200

    Updated the test files to handle new scheduler and created test file to make individual plots.

--- tests/analyse_benchmark.py
+++ tests/analyse_benchmark.py
@@ -4,12 +4,37 @@ os.environ['APPDATA'] = ""
 import pandasgui
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 if __name__ == "__main__":
 
     csv_file = '/home/joris/tu_delft_ws/15_msc_thesis/benchmark_global_planner/2024-05-22_19-39-08.csv'
-
+    
     df = pd.read_csv(csv_file)
 
-    pandasgui.show(df)
+    # Plotting boxplots
+    plt.figure(figsize=(12, 6))
+
+    # Boxplot for run_time
+    plt.subplot(1, 2, 1)
+    sns.boxplot(x='stationary_area', y='run_srm_time', data=df, hue='run_srm_result')
+    plt.title('Boxplot of Run Time vs Stationary Area')
+    plt.xlabel('Stationary Area (%)')
+    plt.ylabel('Run Time')
+    plt.legend(title='Result')
+
+    # Boxplot for run_force
+    plt.subplot(1, 2, 2)
+    sns.boxplot(x='stationary_area', y='run_srm_force', data=df, hue='run_srm_result')
+    plt.title('Boxplot of Run Force vs Stationary Area')
+    plt.xlabel('Stationary Area (%)')
+    plt.ylabel('Run Force')
+    plt.legend(title='Result')
+
+    plt.tight_layout()
+    plt.show()
+
+    # pandasgui.show(df)
